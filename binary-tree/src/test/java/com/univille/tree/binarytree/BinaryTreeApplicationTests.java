9e668ade19e1fa27f32a9fc8524ecb5d6a11e814
Gabrielly Gomes Martinez <41309216+gabrisamartinez@users.noreply.github.com>
2020-10-10 11:40:03 -0300

    estrutura do novo projeto
    apenas a estrutura spring crua do novo trabalho de arvores binárias

---
+++ binary-tree/src/test/java/com/univille/tree/binarytree/BinaryTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univille.tree.binarytree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinaryTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
